69822468da12a5116689ad838f831c611c5d2f17
syllant <syllant@gmail.com>
2008-09-24 20:55:29 +0000

    Initialization

---
+++ src/main/java/org/sylfra/idea/plugins/remotesynchronizer/actions/toolbar/ThreadInterruptAction.java
@@ -0,0 +1,33 @@
+package org.sylfra.idea.plugins.remotesynchronizer.actions.toolbar;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.sylfra.idea.plugins.remotesynchronizer.synchronizing.SynchronizerThread;
+import org.sylfra.idea.plugins.remotesynchronizer.utils.Utils;
+
+/**
+ * Interrupt current synchronization
+ *
+ * @author <a href="mailto=sylfradev@yahoo.fr">Sylvain FRANCOIS</a>
+ */
+public class ThreadInterruptAction extends AnAction
+{
+  public void actionPerformed(AnActionEvent e)
+  {
+    SynchronizerThread t = Utils.getCurrentCopierThread(e);
+
+    if (!t.isInterrupted())
+      t.interrupt();
+  }
+
+  public void update(AnActionEvent e)
+  {
+    SynchronizerThread t = Utils.getCurrentCopierThread(e);
+
+    boolean show = ((t == null) || (!t.isInterrupted()));
+    boolean enabled = ((t != null) && (t.isActive()));
+
+    e.getPresentation().setVisible(show);
+    e.getPresentation().setEnabled(enabled);
+  }
+}
