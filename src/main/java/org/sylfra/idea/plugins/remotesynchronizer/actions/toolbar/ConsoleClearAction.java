ed5fd6d07818a55b32e4914c05d93c8460aaebd9
syllant <syllant@gmail.com>
2008-09-24 20:54:07 +0000

    Initialization

---
+++ src/main/java/org/sylfra/idea/plugins/remotesynchronizer/actions/toolbar/ConsoleClearAction.java
@@ -0,0 +1,31 @@
+package org.sylfra.idea.plugins.remotesynchronizer.actions.toolbar;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.sylfra.idea.plugins.remotesynchronizer.RemoteSynchronizerPlugin;
+import org.sylfra.idea.plugins.remotesynchronizer.ui.ThreadConsole;
+import org.sylfra.idea.plugins.remotesynchronizer.utils.Utils;
+
+/**
+ * Clear the current console
+ *
+ * @author <a href="mailto=sylfradev@yahoo.fr">Sylvain FRANCOIS</a>
+ */
+public class ConsoleClearAction extends AnAction
+{
+  public void actionPerformed(AnActionEvent e)
+  {
+    ThreadConsole currentConsole = Utils.getPlugin(e).getConsolePane()
+      .getCurrentConsole();
+    if (currentConsole != null)
+      currentConsole.clear();
+  }
+
+  public void update(AnActionEvent e)
+  {
+    RemoteSynchronizerPlugin plugin = Utils.getPlugin(e);
+    e.getPresentation().setEnabled((plugin != null)
+      && (plugin.getConsolePane().getCurrentConsole() != null)
+      && (!plugin.getConsolePane().getCurrentConsole().isCleared()));
+  }
+}
